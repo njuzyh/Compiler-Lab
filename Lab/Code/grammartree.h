e5e16f7a536efe6d48aa055b8ff05282e6e8e688
njuzyh <1223870886@qq.com>
2019-04-24 09:22:38 -0500

    Add files via upload

---
+++ Lab/Code/grammartree.h
@@ -0,0 +1,24 @@
+extern int yylineno;
+extern char* yytext;
+
+#ifndef _GRAMMARTREE_H_
+#define _GRAMMARTREE_H_
+
+typedef struct GrammarTreeNode {
+	char* unitName;
+	int lineNo;
+	int lex;
+	char* lexValue;
+	int childrenNum;
+	struct GrammarTreeNode* childrenList[10];
+}GTN;
+
+GTN* newLexNode(char* unitName, int lineNo, char* lexValue);
+
+GTN* newGramNode(char* unitName, int childrenNum, ...);
+
+void outputNode(GTN* gramNode, int tabspace);
+
+void printTabSpace(int tabspaceNum);
+
+#endif
